ab93d4b8217dc6fbbac66afd9d8d1c1179e8306f
ellandri <ahou.405@gmail.com>
2024-10-16 23:23:39 +0000

    first commit

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './presentations/home/home.component';
+import { FaqComponent } from './presentations/faq/faq.component';
+import { MylearningComponent } from './presentations/mylearning/mylearning.component';
+import { NewsComponent } from './presentations/news/news.component';
+import { SimulatorComponent } from './presentations/simulator/simulator.component';
 
 // Component
-
 const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'mylearning', component: MylearningComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'simulator', component: SimulatorComponent },
 ];
 
 @NgModule({
--- app.module.ts
+++ app.module.ts
@@ -1,27 +1,48 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RouterModule, Routes } from '@angular/router'; // Corrigé l'import pour Routes
 // Page Route
 import { AppRoutingModule } from './app-routing.module';
 
 // toaster
 import { ToastrModule } from 'ngx-toastr';
 
-
 // Auth
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
-
-// Component
+// Components
 import { AppComponent } from './app.component';
+import { FaqComponent } from './presentations/faq/faq.component';
+import { HomeComponent } from './presentations/home/home.component';
+import { SimulatorComponent } from './presentations/simulator/simulator.component';
+import { MylearningComponent } from './presentations/mylearning/mylearning.component';
+import { NewsComponent } from './presentations/news/news.component';
+
+import { FullCalendarModule } from '@fullcalendar/angular'; 
+import { AlertModule } from 'ngx-bootstrap/alert';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgxSimplebarModule } from 'ngx-simplebar';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+// import { CalendarComponent } from './presentations/calendar/calendar.component';
 
 
-
+// Définition des routes
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'faq', component: FaqComponent },
+  { path: 'simulator', component: SimulatorComponent },
+  // {path: 'calendar', component: CalendarComponent},
+  { path: 'mylearning', component: MylearningComponent },
+  { path: 'news', component: NewsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     HttpClientModule,
@@ -29,9 +50,23 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     AppRoutingModule,
     ToastrModule.forRoot(),
+    RouterModule.forRoot(routes), // Configuration du routeur ici
+    HomeComponent,
+    FaqComponent,
+    MylearningComponent,
+    NewsComponent,
+    SimulatorComponent,
+    FullCalendarModule,
+    AlertModule.forRoot(),
+    ModalModule.forRoot(),
+    ReactiveFormsModule,
+    FlatpickrModule.forRoot(),
+    NgxSimplebarModule,
+    NgxDropzoneModule,
+
   ],
-  providers: [
-  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
